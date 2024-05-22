80734a974e401a0679a8a38cc1d32bff1872d125
Borko <borkokandic@gmail.com>
2024-05-22 16:33:22 +0200

    pravljenje modela, forme i registrovanje

--- list/models.py
+++ list/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class list(models.Model):
+    knjiga = models.CharField(max_length=100)
+    strana = models.CharField(max_length=1000)
+    broj = models.CharField(max_length=100000)
+
+    eparhija = models.CharField(max_length=100)
+    hram = models.CharField(max_length=100)
+    mjesto = models.CharField(max_length=30)
+    godina = models.CharField(max_length=2)
+
+    datum_rodjenja = models.CharField(max_length=100)
+    mjesto_rodjenja = models.CharField(max_length=30)
+
+    
